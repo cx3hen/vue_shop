1dc20ca010576b1d6b762a6dbad8b185041b1bd5
沈晨旭 <1206726916@qq.com>
2022-05-22 22:39:44 +0800

    完成了订单功能

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Cate from '@/components/goods/Cate'
 import Params from '@/components/goods/Params'
 import GoodsList from '@/components/goods/List'
 import GoodAdd from '@/components/goods/Add'
+import Order from '@/components/order/Order'
 
 Vue.use(VueRouter)
 
@@ -52,6 +53,9 @@ const router = new VueRouter({
       }, {
         path: '/goods/add',
         component: GoodAdd
+      }, {
+        path: '/orders',
+        component: Order
       }]
     }
   ]
