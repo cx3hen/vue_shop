561b4ea3258d4e6b93e59bc47a786160be796695
沈晨旭 <1206726916@qq.com>
2022-05-23 00:43:03 +0800

    完成了报表功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import Params from '@/components/goods/Params'
 import GoodsList from '@/components/goods/List'
 import GoodAdd from '@/components/goods/Add'
 import Order from '@/components/order/Order'
+import Report from '@/components/report/Report'
 
 Vue.use(VueRouter)
 
@@ -56,6 +57,9 @@ const router = new VueRouter({
       }, {
         path: '/orders',
         component: Order
+      }, {
+        path: '/reports',
+        component: Report
       }]
     }
   ]
